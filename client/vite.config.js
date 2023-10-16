6206c790875760c4c9a6f6d6d01959384b6c0ba3
Sam Waters <samuel.waters@gmail.com>
2023-10-16 11:32:05 -0600

    finished

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     // changes our vite to launch out of port 3000
     port: 3000,
-    // this allows the app to be accessed from outside the localhost 
+    // this allows the app to be accessed from outside the localhost
     cors:true,
     // we write our fetches to /api/route and it will go through this proxy
     proxy: {
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,18 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        # db.create_all()
+        for _ in range(15):
+            u = User(
+                username = fake.name(),
+                password_hash = "123abc"
+            )
+            db.session.add(u)
+            db.session.commit()
